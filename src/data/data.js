30354fb8ec6e24c1af108b1319f1b01eb1c67915
Katrina <kalynndier@gmail.com>
2023-04-05 18:01:50 -0400

    photo gallery started

---
+++ src/data/data.js
@@ -0,0 +1,65 @@
+
+export const photos = [
+         {
+           id: 1,
+           title: "one",
+           url:
+             "https://blog.queenbeandesigncafe.com/wp-content/uploads/2023/04/hotCoffee.png",
+         },
+
+         {
+           id: 2,
+           title: "two",
+           url:
+             "https://blog.queenbeandesigncafe.com/wp-content/uploads/2023/04/hotCoffee.png",
+         },
+
+         {
+           id: 3,
+           title: "three",
+           url: "",
+         },
+
+         {
+           id: 4,
+           title: "four",
+           url: "",
+         },
+
+         {
+           id: 5,
+           title: "five",
+           url: "",
+         },
+
+         {
+           id: 6,
+           title: "six",
+           url: "",
+         },
+
+         {
+           id: 7,
+           title: "seven",
+           url: "",
+         },
+
+         {
+           id: 8,
+           title: "eight",
+           url: "",
+         },
+
+         {
+           id: 9,
+           title: "nine",
+           url: "",
+         },
+
+         {
+           id: 10,
+           title: "ten",
+           url: "",
+         },
+       ];
+
--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../../components/layout/Layout'
+import './about.scss'
+
+import {photos} from '../../data/data'
 
 function About() {
-  return (
 
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const next = () => {
+    setCurrentIndex((currentIndex + 1) % photos.length);
+  }
+
+  const prev = () => {
+    setCurrentIndex((currentIndex - 1 + photos.length) % photos.length)
+  }
+  return (
     <Layout>
-        <section className='about'>
-            <h2>ABOUT</h2>
+      <section className="about">
+        <h2>ABOUT KATRINA DIERKING</h2>
+
+        <section className="slider-container">
+          {photos.map((photo) => (
+            <div
+              key={photo.id}
+              className={
+                photos[currentIndex].id === photo.id ? "fade" : "slide-fade"
+              }
+            >
+              <img src={photo.url} alt={photo.title} className="photo" />
+              <div className="caption">{photo.title}</div>
+            </div>
+          ))}
+
+          <button onClick={prev} className="prev">
+            &lt;
+          </button>
+
+          <button onClick={next} className="next">
+            &gt;
+          </button>
+
+          <div className="dots">
+            {photos.map((photo) => (
+              <span
+                key={photo.id}
+                // highlight the dot that corresponds to the current photo
+                className={
+                  photos[currentIndex].id === photo.id ? "dot active" : "dot"
+                }
+                // when the user clicks on a dot, go to the corresponding photo
+                onClick={() => setCurrentIndex(photos.indexOf(photo))}
+              ></span>
+            ))}
+          </div>
         </section>
-    
+      </section>
     </Layout>
-  )
+  );
 }
 
 export default About
