354a7576e04788197bb4ceb84ef6c153ef4606ab
Katrina <kalynndier@gmail.com>
2023-04-05 18:21:56 -0400

    carousel working

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -4,6 +4,9 @@ import './about.scss'
 
 import {photos} from '../../data/data'
 
+
+
+
 function About() {
 
   const [currentIndex, setCurrentIndex] = useState(0)
@@ -20,41 +23,47 @@ function About() {
       <section className="about">
         <h2>ABOUT KATRINA DIERKING</h2>
 
-        <section className="slider-container">
-          {photos.map((photo) => (
-            <div
-              key={photo.id}
-              className={
-                photos[currentIndex].id === photo.id ? "fade" : "slide-fade"
-              }
-            >
-              <img src={photo.url} alt={photo.title} className="photo" />
-              <div className="caption">{photo.title}</div>
-            </div>
-          ))}
+        <section className='slider-container'>
+        {photos.map((photo) => (
+          <div
+            key={photo.id}
 
-          <button onClick={prev} className="prev">
-            &lt;
-          </button>
-
-          <button onClick={next} className="next">
-            &gt;
-          </button>
-
-          <div className="dots">
-            {photos.map((photo) => (
-              <span
-                key={photo.id}
-                // highlight the dot that corresponds to the current photo
-                className={
-                  photos[currentIndex].id === photo.id ? "dot active" : "dot"
-                }
-                // when the user clicks on a dot, go to the corresponding photo
-                onClick={() => setCurrentIndex(photos.indexOf(photo))}
-              ></span>
-            ))}
+            // if the photo is the current photo, show it
+            className={
+              photos[currentIndex].id === photo.id ? 'fade' : 'slide fade'
+            }
+          >
+            <img src={photo.url} alt={photo.title} className='photo' />
+            <div className='caption'>{photo.title}</div>
           </div>
-        </section>
+        ))}
+
+        {/* Previous button */}
+        <button onClick={prev} className='prev'>
+          &lt;
+        </button>
+
+        {/* Next button */}
+        <button onClick={next} className='next'>
+          &gt;
+        </button>
+      </section>
+
+      {/* Render dots indicator */}
+      <section className='dots'>
+        {photos.map((photo) => (
+          <span
+            key={photo.id}
+            // highlight the dot that corresponds to the current photo
+            className={
+              photos[currentIndex].id === photo.id ? 'dot active' : 'dot'
+            }
+            // when the user clicks on a dot, go to the corresponding photo
+            onClick={() => setCurrentIndex(photos.indexOf(photo))}
+          ></span>
+        ))}
+      </section>
+        
       </section>
     </Layout>
   );
