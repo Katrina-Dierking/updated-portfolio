fdf94e6ea594b93d22e5f0675b586dacdd8d984e
Katrina <kalynndier@gmail.com>
2023-04-05 13:17:35 -0400

    project animation

--- src/views/projects/Projects.js
+++ src/views/projects/Projects.js
@@ -4,33 +4,34 @@ import './project.scss'
 function Projects() {
   return (
     <section className="projectsMain">
-    <h3>PROJECTS</h3>
-    <div className='projectGallery'>
+      <div className="projBanner">
+        <h3 className='animate-charcter'>PROJECTS</h3>
+      </div>
+      <div className="projectGallery">
         <div className="project">
-            <img src="#" alt="img" />
-            <h5>Title</h5>
-            <p>Description</p>
-            <a href="#">Link</a>
+          <img src="#" alt="img" />
+          <h5>Title</h5>
+          <p>Description</p>
+          <a href="#">Link</a>
         </div>
         <div className="project">
-            <img src="#" alt="img" />
-            <h5>Title</h5>
-            <p>Description</p>
-            <a href="#">Link</a>
+          <img src="#" alt="img" />
+          <h5>Title</h5>
+          <p>Description</p>
+          <a href="#">Link</a>
         </div>
         <div className="project">
-            <img src="#" alt="img" />
-            <h5>Title</h5>
-            <p>Description</p>
-            <a href="#">Link</a>
+          <img src="#" alt="img" />
+          <h5>Title</h5>
+          <p>Description</p>
+          <a href="#">Link</a>
         </div>
         <div className="project">
-            <img src="#" alt="img" />
-            <h5>Title</h5>
-            <p>Description</p>
-            <a href="#">Link</a>
+          <img src="#" alt="img" />
+          <h5>Title</h5>
+          <p>Description</p>
+          <a href="#">Link</a>
         </div>
-      
       </div>
     </section>
   );
