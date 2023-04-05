06ec4c13e7d3b18ae381c314d4a18d4cbb724c99
Katrina <kalynndier@gmail.com>
2023-04-05 16:36:18 -0400

    dots added

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,40 +3,42 @@ import { Link } from 'react-router-dom'
 import Projects from '../../views/projects/Projects'
 import Layout from '../layout/Layout'
 import './main.scss'
+import coffee from '../../images/hotCoffee.png'
 
 function Main() {
   return (
     <Layout>
-      <section className='main'>
-
-        <section className='mainTop'>
+      <section className="main">
+        <section className="mainTop">
           <h2>KATRINA DIERKING</h2>
-          <p>SOFTWARE ENGINEER | MASTER CRAFTER | COFFEE ADDICT</p>
+
+          <p>
+            SOFTWARE ENGINEER
+            <li> MASTER CRAFTER</li>
+            <li> COFFEE ADDICT</li>
+          </p>
         </section>
 
-        <section className='mainBottom'>
-          <div className='butLeft'>
-            <Link to='/contact'>
+        <section className="mainBottom">
+          <div className="butLeft">
+            <Link to="/contact">
               <button>NEED A WEBSITE?</button>
             </Link>
           </div>
 
-          <div className='butRight'>
-            <Link to='/resume'>
+          <div className="butRight">
+            <Link to="/resume">
               <button>NEED A DEVELOPER?</button>
             </Link>
           </div>
         </section>
-
-        
-        </section>
-        <section className='quote effect2'>
-          <h3>Let's build something great together!</h3>
-        </section>
-        <Projects />
-
+      </section>
+      <section className="quote effect2">
+        <h3>Let's build something great together!</h3>
+      </section>
+      <Projects />
     </Layout>
-  )
+  );
 }
 
 export default Main
