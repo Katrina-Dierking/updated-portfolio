e027821c10773a1be580ef9bc665145a0e7a753d
Katrina <kalynndier@gmail.com>
2023-04-05 16:12:50 -0400

    footer links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './footer.scss'
 
 function Footer() {
   return (
     <footer>
       <nav className="footerNav">
-        <a href="https://google.com">CONTACT ME</a>
-        <a href="https://google.com">NEED A DEVELOPER?</a>
-        <a href = "#">NEED A WEBSITE?</a>
+        <Link to="/resume">NEED A DEVELOPER?</Link>
+        ||
+        <Link to="/contact">NEED A WEBSITE?</Link>
       </nav>
       <p>Copyright © 2023 | Katrina Dierking | Queen Bean Design Cafe</p>
     </footer>
