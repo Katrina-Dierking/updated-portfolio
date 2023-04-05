34b1cdbdb0f3313e834bbfbcad17398e4585d9a8
Katrina <kalynndier@gmail.com>
2023-04-05 14:07:33 -0400

    nav bar updated

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import './nav.scss'
 
+import home from '../../images/home.png'
+import about from '../../images/about.png'
+import projects from '../../images/projects.png'
+import resume from '../../images/resume.png'
+
 export const Nav = () => {
   return (
-    <nav className='topNav'>
-      <a href="https://google.com">HOME</a>
-      <a href="https://google.com">ABOUT</a>
-      <a href="https://google.com">PROJECTS</a>
-      <a href="https://google.com">RESUME</a>
+    <nav className="topNav container">
+      <div className="container">
+        <div className="profile">
+          <img src={home} alt="home icon" />
+          <a href="https://google.com" className="title">
+            HOME
+          </a>
+        </div>
+
+        <div className="profile">
+          <img src={about} alt="about icon" />
+          <a href="https://google.com" className="title">
+            ABOUT
+          </a>
+        </div>
+
+        <div className="profile">
+          <img src={projects} alt="projects icon" />
+          <a href="https://google.com" className="title">
+            PROJECTS
+          </a>
+        </div>
+
+        <div className="profile">
+          <img src={resume} alt="resume icon" />
+          <a href="https://google.com" className="title">
+            RESUME
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
